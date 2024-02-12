02c3df0f2d81d4b7a58c28dedd444445b889030b
Michael Foster <m.foster@sheffield.ac.uk>
2024-02-12 15:48:57 +0000

    Privileged driver

---
+++ leaderboard/scripts/privileged_driver.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# Roach data collection
+while getopts "c:v:s:r:" flag; do
+  case "$flag" in
+    c) carlaVersion=$OPTARG;;
+    v) egoVehicle=$OPTARG;;
+    s) scenarioIndex=${OPTARG};;
+    r) routeIndex=$OPTARG;;
+  esac
+done
+
+carlaVersion=${carlaVersion:-10}
+egoVehicle=${egoVehicle:-vehicle.lincoln.mkz2017}
+
+export CARLA_ROOT="../CARLA-$carlaVersion"
+export WORK_DIR=.
+
+export EGO_VEHICLE=$egoVehicle
+export CARLA_SERVER=${CARLA_ROOT}/CarlaUE4.sh
+export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI
+export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI/carla
+export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI/carla/dist/carla-0.9.${carlaVersion}-py3.7-linux-x86_64.egg
+export SCENARIO_RUNNER_ROOT=${WORK_DIR}/scenario_runner
+export LEADERBOARD_ROOT=${WORK_DIR}/leaderboard
+export PYTHONPATH="${CARLA_ROOT}/PythonAPI/carla/":"${SCENARIO_RUNNER_ROOT}":"${LEADERBOARD_ROOT}":${PYTHONPATH}
+echo $PYTHONPATH
+
+# export ROUTES=${WORK_DIR}/leaderboard/data/longest6.xml
+export ROUTES=${WORK_DIR}/leaderboard/data/citcom_data/Town01_Scenario${scenarioIndex}/route_${routeIndex}.xml
+export SCENARIOS=${WORK_DIR}/leaderboard/data/training/scenarios/s${scenarioIndex}/Town01_Scenario${scenarioIndex}.json
+
+function join_by {
+  local d=${1-} f=${2-}
+  if shift 2; then
+    printf %s "$f" "${@/#/$d}"
+  fi
+}
+
+SAVE_DIR=$(join_by _ ${@:1:$OPTIND})
+export SAVE_PATH="${WORK_DIR}/privileged/${SAVE_DIR//-/""}"
+export CHECKPOINT_ENDPOINT=${SAVE_PATH}/data_collect_town01_results.json
+
+export REPETITIONS=1
+export CHALLENGE_TRACK_CODENAME=MAP
+export TEAM_AGENT=${WORK_DIR}/team_code/data_agent.py
+export TEAM_CONFIG=""
+export DEBUG_CHALLENGE=0
+export RESUME=1
+export DATAGEN=1
+# export BENCHMARK=longest6
+export UNCERTAINTY_THRESHOLD=0.33
+export STOP_CONTROL=1
+export DIRECT=0
+
+
+
+python3 ${LEADERBOARD_ROOT}/leaderboard/leaderboard_evaluator_local.py \
+--scenarios=${SCENARIOS}  \
+--routes=${ROUTES} \
+--repetitions=${REPETITIONS} \
+--track=${CHALLENGE_TRACK_CODENAME} \
+--checkpoint=${CHECKPOINT_ENDPOINT} \
+--agent=${TEAM_AGENT} \
+--agent-config=${TEAM_CONFIG} \
+--debug=0 \
+--resume=${RESUME} \
+--timeout=600
---
+++ leaderboard/scripts/run_routes_scenarios_privileged.sh
@@ -0,0 +1,14 @@
+for s in 1 3 4 7 8 9
+do
+    for f in leaderboard/data/citcom_data/Town01_Scenario${s}/*.xml
+    do
+      split=$(echo $f | tr "_" "\n")
+      split=($split)
+      split=$(echo ${split[3]} | tr "." "\n")
+      split=($split)
+      r=${split[0]}
+      bash leaderboard/scripts/privileged_driver.sh -c 11 -v vehicle.lincoln.mkz2017 -s $s -r $r
+      bash leaderboard/scripts/privileged_driver.sh -c 11 -v vehicle.bmw.isetta -s $s -r $r
+      find privileged/ -name "route_*" -type d -exec rm {} \;
+    done
+done
